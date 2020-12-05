d54225a92a846952260c675e010acb5763718c93
junggu.ji <junggu.ji.dev@gmail.com>
2020-12-05 19:17:14 +0900

    type : init
    프로젝트 생성

---
+++ src/main/java/com/junggu/backjoonframeautomaticgenerator/BackjoonFrameAutomaticGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.junggu.backjoonframeautomaticgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackjoonFrameAutomaticGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackjoonFrameAutomaticGeneratorApplication.class, args);
+	}
+
+}
