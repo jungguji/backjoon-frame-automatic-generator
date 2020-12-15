80122d2932ab745c55b0ba754f4b92d718ebd7e0
junggu.ji <junggu.ji.dev@gmail.com>
2020-12-15 16:31:16 +0900

    type : feat
    * 공통으로 사용될 axios 헤더 생성

---
+++ frontend/src/api/common.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const AXIOS = axios.create({
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json; charset = utf-8"
+  },
+  timeout: 1000
+});
