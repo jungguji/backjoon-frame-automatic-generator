63833187be1231dfa09bdee04befb37dd3250a4f
junggu.ji <junggu.ji.dev@gmail.com>
2020-12-21 19:01:09 +0900

    type : fix
    * AXIOS 사용 할 수 있도록 export 추가

--- frontend/src/api/common.js
+++ frontend/src/api/common.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const AXIOS = axios.create({
+export const AXIOS = axios.create({
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json; charset = utf-8'
