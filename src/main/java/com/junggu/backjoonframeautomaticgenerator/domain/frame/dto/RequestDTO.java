bef982d51adcde8e9f69c2a3b66d1ac6dd476621
junggu.ji <junggu.ji.dev@gmail.com>
2020-12-29 14:46:05 +0900

    type : fix
    * axios 데이터 전달 받기 위해 기본 자료형 변수 boolean을 참조형 Boolean 변수로 변경

--- src/main/java/com/junggu/backjoonframeautomaticgenerator/domain/frame/dto/RequestDTO.java
+++ src/main/java/com/junggu/backjoonframeautomaticgenerator/domain/frame/dto/RequestDTO.java
@@ -7,14 +7,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class RequestDTO {
-    private boolean isTestCase;
-    private boolean isNQuantity;
-    private boolean isSpaceIncludeNumber;
+    private Boolean isTestCase;
+    private Boolean isNQuantity;
+    private Boolean isSpaceIncludeNumber;
 
     @Builder
-    public RequestDTO(boolean isTestCase, boolean isNQuantity, boolean isSpaceIncludeNumber) {
+    public RequestDTO(Boolean isTestCase, Boolean isNQuantity, Boolean isSpaceIncludeNumber) {
         this.isTestCase = isTestCase;
         this.isNQuantity = isNQuantity;
         this.isSpaceIncludeNumber = isSpaceIncludeNumber;
+    }
+
+    public String toString() {
+        return "tsetcase >> " + this.isTestCase + " nQ >> " + isNQuantity + " space >> " + isSpaceIncludeNumber;
     }
 }
