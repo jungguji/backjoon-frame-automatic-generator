53ec824500a93a44c2508bf46b837e819d53fd01
junggu.ji <junggu.ji.dev@gmail.com>
2020-12-29 14:15:33 +0900

    type : fix
    * 데이터를 전송 받기 위해 post 방식으로 변경

--- frontend/src/api/template/request.js
+++ frontend/src/api/template/request.js
@@ -2,13 +2,7 @@ import { AXIOS } from '../common.js'
 
 function requestTemplate (template) {
   alert(template.isTestCase)
-  return AXIOS.get(`${process.env.VUE_APP_BASEURL}/frame`, {
-    params: {
-      isTestCase: template.isTestCase,
-      isNQuantity: template.isNQuantity,
-      isSpaceIncludeNumber: template.isSpaceIncludeNumber
-    }
-  })
+  return AXIOS.post(`${process.env.VUE_APP_BASEURL}/frame`, 'test')
 }
 
 export { requestTemplate }
