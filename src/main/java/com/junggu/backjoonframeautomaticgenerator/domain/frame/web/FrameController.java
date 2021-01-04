07c6a835695040e20bc11a5468b5ded3fea12af4
junggu.ji <junggu.ji.dev@gmail.com>
2021-01-04 12:09:41 +0900

    type : feat
    * 체크한 값이 따라 template code 리턴

---
+++ src/main/java/com/junggu/backjoonframeautomaticgenerator/domain/frame/web/FrameController.java
@@ -0,0 +1,26 @@
+package com.junggu.backjoonframeautomaticgenerator.domain.frame.web;
+
+import com.junggu.backjoonframeautomaticgenerator.domain.frame.domain.Frame;
+import com.junggu.backjoonframeautomaticgenerator.domain.frame.dto.RequestDTO;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FrameController {
+
+    @PostMapping(path = "/frame", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getFrame(@RequestBody RequestDTO request) {
+        System.out.println(request.toString());
+        Frame frame = Frame.builder()
+                .isTestCase(request.isTestCase())
+                .isNQuantity(request.isMultiCaseQuantity())
+                .isSpaceIncludeNumber(request.isSpaceIncludeNumber())
+                .build();
+
+        String test = frame.getForm();
+        System.out.println(test);
+        return test;
+    }
+}
