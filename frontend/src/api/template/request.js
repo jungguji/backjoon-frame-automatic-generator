b616ee5832e378d1ad35f25b050e546175ef6176
junggu.ji <junggu.ji.dev@gmail.com>
2021-01-04 11:23:01 +0900

    type : fix
    * 불필요한 alert 제거

--- frontend/src/api/template/request.js
+++ frontend/src/api/template/request.js
@@ -1,7 +1,6 @@
 import { AXIOS } from '../common.js'
 
 function requestTemplate (template) {
-  alert(template.isTestCase)
   return AXIOS.post(`${process.env.VUE_APP_BASEURL}/frame`, template)
 }
 
