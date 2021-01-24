5794fa749bb79da40b0772ca0c3147124247c284
junggu.ji <junggu.ji.dev@gmail.com>
2021-01-24 17:08:20 +0900

    chore : vue.js 빌드

--- backend/src/main/java/com/junggu/backjoonframeautomaticgenerator/BackjoonFrameAutomaticGeneratorApplication.java
+++ backend/src/main/java/com/junggu/backjoonframeautomaticgenerator/BackjoonFrameAutomaticGeneratorApplication.java
@@ -9,5 +9,4 @@ public class BackjoonFrameAutomaticGeneratorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackjoonFrameAutomaticGeneratorApplication.class, args);
 	}
-
 }
