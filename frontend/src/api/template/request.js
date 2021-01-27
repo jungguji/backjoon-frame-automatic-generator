d20fbbb6f9528a4b766e09ec1ef1ba985299b3e7
junggu.ji <junggu.ji.dev@gmail.com>
2021-01-27 22:47:56 +0900

    refactor : alert remove

--- frontend/src/api/template/request.js
+++ frontend/src/api/template/request.js
@@ -1,7 +1,6 @@
 import { AXIOS } from '../common.js'
 
 function requestTemplate (template) {
-  alert(`${process.env.VUE_APP_BASEURL}`)
   return AXIOS.post(`${process.env.VUE_APP_BASEURL}/frame`, template)
 }
 
